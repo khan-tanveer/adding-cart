9aa5ba99d1cd6926fff294ec9acc8ffde13ecc0c
khan Tanveer <kt224945@gmail.com>
2021-05-04 14:59:34 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>Increment/Decrement Counter</h1>
+      <p>using React and Redux</p>
+      <h1>0</h1>
+      <button>+</button>
+      <button>-</button>
     </div>
   );
 }
