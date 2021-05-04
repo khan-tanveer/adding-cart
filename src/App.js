563087f5b9ef8d3af69d0dfac2803e7324aab576
khan Tanveer <kt224945@gmail.com>
2021-05-04 14:56:02 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>hello redux...............</h1>
+      <h1>Increment/Decrement Counter</h1>
     </div>
   );
 }
