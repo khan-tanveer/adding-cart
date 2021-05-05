0fd3a1c357d78472335f41778374af057ca7f634
khan Tanveer <kt224945@gmail.com>
2021-05-05 19:54:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { incNumber, decNumber } from "./actions/index";
 
 function App() {
+  const myState = useSelector((state) => state.changeTheNumber);
+
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <h1>Increment/Decrement Counter</h1>
       <p>using React and Redux</p>
-      <h1>0</h1>
-      <button>+</button>
-      <button>-</button>
+      <h1>{myState}</h1>
+      <button onClick={() => dispatch(incNumber(5))}> + </button>
+      <button onClick={() => dispatch(decNumber(5))}> - </button>
     </div>
   );
 }
